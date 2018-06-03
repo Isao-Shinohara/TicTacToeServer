576949d601dff0579049221a49944b481d48cade
Isao-Shinohara <shinohara@double-rabbits.net>
2018-06-04 01:42:14 +0900

    create Service Layer

---
+++ TicTacToeServer/Hubs/SignalRClientMessage.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace TicTacToeServer.Hubs
+{
+	public class SignalRClientMessage
+	{
+		public List<string> Clients { get; private set; }
+		public string Method { get; private set; }
+		public object[] Argument { get; private set; }
+
+		public SignalRClientMessage(List<string> clients, string method)
+		{
+			Clients = clients;
+			Method = method;
+		}
+
+		public SignalRClientMessage(List<string> clients, string method, object[] argument)
+		{
+			Clients = clients;
+			Method = method;
+			Argument = argument;
+		}
+
+		public SignalRClientMessage(List<string> clients, string method, object arg1)
+		{
+			Clients = clients;
+			Method = method;
+			var argument = new object[] { arg1 };
+			Argument = argument;
+		}
+
+		public SignalRClientMessage(List<string> clients, string method, object arg1, object arg2)
+		{
+			Clients = clients;
+			Method = method;
+			var argument = new object[] { arg1, arg2 };
+			Argument = argument;
+		}
+
+		public SignalRClientMessage(List<string> clients, string method, object arg1, object arg2, object arg3)
+		{
+			Clients = clients;
+			Method = method;
+			var argument = new object[] { arg1, arg2, arg3 };
+			Argument = argument;
+		}
+
+		public SignalRClientMessage(List<string> clients, string method, object arg1, object arg2, object arg3, object arg4)
+		{
+			Clients = clients;
+			Method = method;
+			var argument = new object[] { arg1, arg2, arg3, arg4 };
+			Argument = argument;
+		}
+
+		public bool IsSingleClient
+		{
+			get {
+				return Clients.Count == 1;
+			}
+		}
+
+		public bool IsMultiClient
+		{
+			get {
+				return Clients.Count > 1;
+			}
+		}
+
+		public bool HaveArgument
+		{
+			get {
+				return Argument != null;
+			}
+		}
+	}
+}
--- TicTacToeServer/Hubs/SignalRHub.cs
+++ TicTacToeServer/Hubs/SignalRHub.cs
@@ -4,47 +4,55 @@ using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
 using TicTacToeServer.Infrastructures;
+using TicTacToeServer.Services;
 
 namespace TicTacToeServer.Hubs
 {
 	public class SignalRHub : Hub
 	{
-		private readonly SignalRContext signalRContext;
+		AppService appService;
 
 		public SignalRHub(SignalRContext context)
 		{
-			this.signalRContext = context;
+			appService = new AppService(context);
 		}
 
 		public override Task OnConnectedAsync()
 		{
 			Console.WriteLine("OnConnected " + Context.ConnectionId);
-
-			if (!this.signalRContext.SignalRItemList.Any(item => item.ConnectionId == Context.ConnectionId)) {
-				this.signalRContext.Update(new SignalRItem { ConnectionId = Context.ConnectionId });
-				this.signalRContext.SaveChanges();
-			}
-
+			appService.AddPlayer(Context.ConnectionId);
 			return base.OnConnectedAsync();
 		}
 
 		public override Task OnDisconnectedAsync(Exception e)
 		{
 			Console.WriteLine("OnDisconnected " + Context.ConnectionId);
-
-			SignalRItem signalRItem = this.signalRContext.SignalRItemList.FirstOrDefault(item => item.ConnectionId == Context.ConnectionId);
-			if (signalRItem != null) {
-				this.signalRContext.Remove(signalRItem);
-				this.signalRContext.SaveChanges();
-			}
-
+			appService.RemovePlayer(Context.ConnectionId);
 			return base.OnDisconnectedAsync(e);
 		}
 
-		public void Send(string message)
+		public void StartSingleGame()
 		{
-			Console.WriteLine(String.Format("[Called '{0}'] {1}", MethodBase.GetCurrentMethod().Name, message));
-			Clients.All.SendAsync(message);
+			Console.WriteLine(String.Format("[Called '{0}']", MethodBase.GetCurrentMethod().Name));
+			var message = appService.StartSingleGame(Context.ConnectionId);
+			CallClientMethod(message);
+		}
+
+		public void CreateRoom(int roomId)
+		{
+			Console.WriteLine(String.Format("[Called '{0}'] {1}", MethodBase.GetCurrentMethod().Name, roomId));
+			var message = appService.CreateRoom(Context.ConnectionId, roomId);
+			CallClientMethod(message);
+		}
+
+		void CallClientMethod(SignalRClientMessage message)
+		{
+			if(!message.HaveArgument)
+			{
+				Clients.Clients(message.Clients).SendAsync(message.Method);
+			} else {
+				Clients.Clients(message.Clients).SendCoreAsync(message.Method, message.Argument);
+			}
 		}
 	}
 }
