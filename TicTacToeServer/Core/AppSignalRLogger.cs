42c4ea068557a34e2f46ba57f34996dd6c25808a
Isao-Shinohara <shinohara@double-rabbits.net>
2018-06-08 01:59:39 +0900

    add AppSignalRLogger

---
+++ TicTacToeServer/Core/AppSignalRLogger.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace TicTacToeServer.Core
+{
+	public static class AppSignalRLogger
+	{
+		public static AppSignalRLogger.Loglevels Level { get; set; } = AppSignalRLogger.Loglevels.Debug;
+
+		public static void LogVerbose(string format, params object[] args)
+		{
+			if (AppSignalRLogger.Level > AppSignalRLogger.Loglevels.Verbose) return;
+			Console.WriteLine(string.Format(format, args));
+		}
+
+		public static void Log(string format, params object[] args)
+		{
+			if (AppSignalRLogger.Level > AppSignalRLogger.Loglevels.Debug) return;
+			Console.WriteLine(string.Format(format, args));
+		}
+
+		public static void LogWarning(string format, params object[] args)
+		{
+			if (AppSignalRLogger.Level > AppSignalRLogger.Loglevels.Warning) return;
+			Console.WriteLine(string.Format(format, args));
+		}
+
+		public static void LogError(string format, params object[] args)
+		{
+			if (AppSignalRLogger.Level > AppSignalRLogger.Loglevels.Error) return;
+			Console.WriteLine(string.Format(format, args));
+		}
+
+		public enum Loglevels : byte
+		{
+			Verbose,
+			Debug,
+			Warning,
+			Error,
+			None,
+		}
+	}
+
+}
--- TicTacToeServer/Hubs/SignalRHub.cs
+++ TicTacToeServer/Hubs/SignalRHub.cs
@@ -2,6 +2,7 @@
 using System.Reflection;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.SignalR;
+using TicTacToeServer.Core;
 using TicTacToeServer.Infrastructures;
 using TicTacToeServer.Services;
 
@@ -13,33 +14,34 @@ namespace TicTacToeServer.Hubs
 
 		public SignalRHub(SignalRContext context)
 		{
+			AppSignalRLogger.Level = AppSignalRLogger.Loglevels.Debug;
 			appService = new AppService(context);
 		}
 
 		public override Task OnConnectedAsync()
 		{
-			Console.WriteLine("OnConnected " + Context.ConnectionId);
+			AppSignalRLogger.Log("OnConnected {0}", Context.ConnectionId);
 			appService.AddPlayer(Context.ConnectionId);
 			return base.OnConnectedAsync();
 		}
 
 		public override Task OnDisconnectedAsync(Exception e)
 		{
-			Console.WriteLine("OnDisconnected " + Context.ConnectionId);
+			AppSignalRLogger.Log("OnDisconnectedAsync {0}", Context.ConnectionId);
 			appService.RemovePlayer(Context.ConnectionId);
 			return base.OnDisconnectedAsync(e);
 		}
 
 		public void StartSingleGame()
 		{
-			Console.WriteLine(String.Format("[Called '{0}']", MethodBase.GetCurrentMethod().Name));
+			AppSignalRLogger.LogVerbose("[Called '{0}']", MethodBase.GetCurrentMethod().Name);
 			var message = appService.StartSingleGame(Context.ConnectionId);
 			CallClientMethod(message);
 		}
 
 		public void CreateRoom(int roomId)
 		{
-			Console.WriteLine(String.Format("[Called '{0}'] {1}", MethodBase.GetCurrentMethod().Name, roomId));
+			AppSignalRLogger.LogVerbose("[Called '{0}'] {1}", MethodBase.GetCurrentMethod().Name, roomId);
 			var message = appService.CreateRoom(Context.ConnectionId, roomId);
 			CallClientMethod(message);
 		}
